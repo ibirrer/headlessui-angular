803f6d849a7c6388dc58489d35a9821c47c3a9c5
ibirrer <ibirrer@gmail.com>
2021-01-03 17:49:25 +0100

    github actions

--- projects/headlessui-angular/karma-ci.conf.js
+++ projects/headlessui-angular/karma-ci.conf.js
@@ -38,9 +38,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    autoWatch: false,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
     restartOnFileChange: true
   });
 };
