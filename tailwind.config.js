1fa95023f534bee5ac58ec3a5096f7f822c495dd
ibirrer <ibirrer@gmail.com>
2022-05-30 20:47:02 +0200

    upgrade to angular 13

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 module.exports = {
-  purge: [
-    './projects/demo/src/**/*.html',
-    './projects/demo/src/**/*.js',
-    './projects/demo/src/**/*.ts',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./projects/demo/src/**/*.{js,ts,html}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
